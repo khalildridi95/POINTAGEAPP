d58285260f57db1ec55aff82f00bbacc0df79832
khalildridi95 <dridi.khalil1@outlook.fr>
2025-12-29 11:41:56 +0100

    Update allowed origins for CORS configuration

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ const JWT_SECRET = process.env.JWT_SECRET || "24d06f19a839638d203ae0fbb3e7ce27";
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://pointage-oeax.onrender.com",
-  "https://khalildridi95.github.io"
+  "http://127.0.0.1:3000",
+  "http://127.0.0.1:5173",
+  "https://khalildridi95.github.io",
+  "https://pointage-oeax.onrender.com"
 ];
 
 // --- DB ---
